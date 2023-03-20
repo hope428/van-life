5d52f36e6307243ac02f4a6922f3c21e623a6007
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-20 13:44:06 -0400

    setup initial files and structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
