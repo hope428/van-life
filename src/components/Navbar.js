d38d008e67c9b4820d2867d96e9f582bee251bfe
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-14 13:57:42 -0400

    added vans link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar(){
             <Link className="site-logo" to="/">#VANLIFE</Link>
             <nav>
                 <Link to="/about">About</Link>
+                <Link to="/vans">Vans</Link>
             </nav>
         </header>
     )
