2f599cf1961338a32aadf05fb9ddc25aa025ec5e
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-14 17:28:26 -0400

    on van page, added code for pulling van from param id to get van info from mirage mock database

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Vans from "./pages/Vans";
+import Van from "./pages/Van";
 
 import './server'
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/vans" element={<Vans />}></Route>
+        <Route path="/vans/:id" element={<Van />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Single-Van.js
+++ src/components/Single-Van.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SingleVan({ van }) {
   return (
     <div className="van-tile">
-      <img src={van.imageUrl}/>
+      <img alt={`${van.name} outside`} src={van.imageUrl}/>
       <div className="van-info">
-        <h3>{van.name}</h3>
+        <h3><Link to={`/vans/${van.id}`}>{van.name}</Link></h3>
         <p>${van.price}<span>/day</span></p>
       </div>
       <i className={`van-type ${van.type} selected`}>{van.type}</i>
