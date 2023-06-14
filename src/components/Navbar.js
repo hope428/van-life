c93f79bb6967aa597e80df70f13664f548b2c073
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-14 13:24:49 -0400

    added images and cleaned up navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import classes from "./Navbar.module.css"
 
 export default function Navbar(){
     return (
-        <nav>
-            <h1 className={classes.logo}><Link to="/">#VANLIFE</Link></h1>
-            <ul className="links">
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/vans">Vans</Link></li>
-            </ul>
-        </nav>
+        <header>
+            <Link className="site-logo" to="/">#VANLIFE</Link>
+            <nav>
+                <Link to="/about">About</Link>
+            </nav>
+        </header>
     )
 }
