c854c6c22827e73c26ea105bb54c5cd0d03e168d
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-14 17:36:28 -0400

    created a layout route for nesting routes. imported oulet from react router to render child routes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Navbar from "./components/Navbar";
 import Vans from "./pages/Vans";
 import Van from "./pages/Van";
+import Layout from "./components/Layout";
 
-import './server'
+import "./server";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/vans" element={<Vans />}></Route>
-        <Route path="/vans/:id" element={<Van />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/vans" element={<Vans />}></Route>
+          <Route path="/vans/:id" element={<Van />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
